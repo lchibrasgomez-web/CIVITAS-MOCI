28d04188aecdbfb89dd216d175823e9f42b14a85
luis chibras gomez <lchibrasgomez@gmail.com>
2026-04-06 16:57:46 -0600

    pro backend fotos

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-import json
-import os
+import json, os, shutil, uuid
 
 app = FastAPI()
 
-# 🔥 CORS TOTAL
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,7 +14,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 🔥 BASE DE DATOS SIMPLE (archivo)
+# carpeta imágenes
+if not os.path.exists("imagenes"):
+    os.makedirs("imagenes")
+
+app.mount("/imagenes", StaticFiles(directory="imagenes"), name="imagenes")
+
 DB = "db.json"
 
 if not os.path.exists(DB):
@@ -34,6 +38,8 @@ class Reporte(BaseModel):
     descripcion: str
     lat: float
     lng: float
+    tipo: str = "General"
+    foto: str = ""
 
 @app.get("/")
 def home():
@@ -42,6 +48,16 @@ def home():
 @app.get("/reportes")
 def get_reportes():
     return leer()
+
+@app.post("/subir-imagen")
+async def subir_imagen(file: UploadFile = File(...)):
+    nombre = f"{uuid.uuid4()}.jpg"
+    ruta = f"imagenes/{nombre}"
+
+    with open(ruta, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"url": f"https://civitas-moci-production.up.railway.app/imagenes/{nombre}"}
 
 @app.post("/reportes")
 def post_reporte(r: Reporte):
